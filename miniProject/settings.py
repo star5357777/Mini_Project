555c869f9a4cb9bf1d3763bf7ad91c55cc4c260c
krystal <star535@nate.com>
2023-04-19 15:53:56 +0900

    secret_key 설정 및 초기 migrate 설정 완료

--- miniProject/settings.py
+++ miniProject/settings.py
@@ -9,10 +9,10 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-import os
+import os, environ
 from pathlib import Path
 
-import environ
+
 env = environ.Env(
     # set casting, default value
     DEBUG=(bool, False)
@@ -21,6 +21,7 @@ env = environ.Env(
 environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 environ.Env.read_env(
@@ -31,7 +32,7 @@ environ.Env.read_env(
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
