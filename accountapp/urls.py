7b94434df43af4e514325b97800b74d4cae67965
krystal <star535@nate.com>
2023-04-20 11:32:51 +0900

    user 로그인 상태, 로그인, 가입 home 리다이렉트 처리

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,13 +1,13 @@
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 
 
-from accountapp.views import UserCreateView, UserDetailView
+from accountapp.views import UserCreateView, UserDetailView, CustomLoginView
 
 app_name = 'accountapp'
 
 urlpatterns = [
-    path('login/', LoginView.as_view(template_name='accountapp/login.html'), name='login'),
+    path('login/', CustomLoginView.as_view(template_name='accountapp/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('create/', UserCreateView.as_view(), name='create'),
     path('detail/<int:pk>', UserDetailView.as_view(), name='detail'),
--- accountapp/views.py
+++ accountapp/views.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.shortcuts import render
+from django.contrib.auth.views import LoginView, LogoutView
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, TemplateView
@@ -23,7 +25,18 @@ class UserCreateView(CreateView):
     success_url = reverse_lazy('home')
     template_name = 'accountapp/create.html'
 
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('home')
+        return super().dispatch(request, *args, **kwargs)
+
 class UserDetailView(DetailView):
     model = CustomUser
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
+
+class CustomLoginView(LoginView):
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('home')
+        return super().dispatch(request, *args, **kwargs)
